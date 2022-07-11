8b4f437efc275426078687fe1bbb12dfe5bd6e12
lucho9595 <lucianocoronel48@gmail.com>
2022-07-11 16:32:22 -0300

    PI- Rick and Morty

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -4,6 +4,30 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("character", {
-
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+type: DataTypes.STRING,
+allowNull: false
+    },
+    species: {
+      type: DataType.ENUM,
+      allowNull: false,
+      value: ['Human', 'Alien']
+    },
+    origin: {
+      type: DataType.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataType.STRING,
+      allowNull: false
+    },
+    created: {
+      type: DataType.DATE,
+      allowNull: false
+    }
   });
 };
